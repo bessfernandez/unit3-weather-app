a8cd72af7194a69a139969d6c3bdd910534a88b6
Rose Emmons <rose.emmons@mac.com>
2013-06-21 11:07:32 -0700

    Added color transitions and placeholders for additional weather data.

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,43 @@
 $(document).ready(function($) {
+function runProgram(){
+	
+	// Set lines 5 - 7 so the DOM is scanned only once.
+	var locationText = $(".location-text");
+	var circle = $("header");
+	var temp = $("#temp");
+	var colors = ["cold", "frozen", "hot", "lukewarm", "unfilled", "warm", "white-text"]; //See css.css lines 160 - 208
+
+	function setNewClass(arg){
+		circle.removeClass(colors.join(" "));
+		temp.addClass("white-text");
+		circle.addClass(arg);
+	}
+
+		
+	function setBackgroundColor(arg) {
+		if(arg <= 120 && arg >= 90){
+			setNewClass("hot");
+		}
+		else if(arg <= 89 && arg >= 80){
+			setNewClass("warm");
+		}
+		else if(arg <= 79 && arg >= 60){
+			setNewClass("lukewarm");
+		}
+		else if(arg <= 59 && arg >= 33){
+			setNewClass("cold");
+		}
+		else {
+			setNewClass("frozen");
+		}
+	}
+	
+	
 
 	function getCurrentTemp(){
 
-		var apikey = "784bfc9f8a6f1730";
+		var apikey = "784bfc9f8a6f1730";  //Made the API Key a variable in case I need to generate a new key.
+		
 		$(".more-data").hide();
 
 		//This grabs the user's location from the browser:
@@ -14,8 +49,8 @@ $(document).ready(function($) {
 			    var userLocation = {};
 			    userLocation.city = jsonObj.location.city;
 			    userLocation.state = jsonObj.location.state;
-			    userLocation.country = jsonObj.location.country;
-			    $(".location-text").text(userLocation.city+", "+userLocation.state+" "+userLocation.country);
+			    userLocation.country = jsonObj.location.country;				
+			    locationText.text(userLocation.city+", "+userLocation.state+" "+userLocation.country);
 		    
 			    //This uses the broswer location data to pull in weather data:
 			    $.ajax({
@@ -23,13 +58,26 @@ $(document).ready(function($) {
 			    	dataType: "jsonp",
 			    	cache: false,
 			    	success : function(jsonObj){
+						userLocation.relativeHumidity = jsonObj.location.relative_humidity;
+						userLocation.weather = jsonObj.location.weather;
+						userLocation.windString = jsonObj.location.wind_string;
+						userLocation.pressureIn = jsonObj.location.pressure_in;
+						userLocation.dewpointF = jsonObj.location.dewpoint_f;
+						userLocation.visibilityMi = jsonObj.location.visibility_mi;
+						userLocation.uv = jsonObj.location.UV;
+						userLocation.precipTodayIn = jsonObj.location.precip_today_in;
+						
 			    		var currentTempF = Math.ceil(jsonObj.current_observation.temp_f);
-			    		$("#temp").html(currentTempF+"&deg");
+						setBackgroundColor(currentTempF);
+			    		temp.html(currentTempF+"&deg");
+			
+						$(".more-data").append();
+			
 			      	},
 					error : function(result) {
 						if (result.statusText != "abort") {  //Won't show the error message if the Ajax call was aborted.
-							$("#temp").text("timed out");
-							$(".location-text").text("We were unable to grab weather data. We apologize for the inconvenience.");
+							temp.text("timed out");
+							locationText.text("We were unable to grab weather data. We apologize for the inconvenience.");
 		            	}
 					}
 			    });//End inner Ajax call
@@ -37,12 +85,15 @@ $(document).ready(function($) {
 
 			error : function(result) {
 				if (result.statusText != "abort") {  //Won't show the error message if the Ajax call was aborted.
-					$("#temp").text("timed out");
-					$(".location-text").text("We were unable to grab your location data.  We apologize for the inconvenience.");
+					temp.text("timed out");
+					locationText.text("We were unable to grab your location data.  We apologize for the inconvenience.");
 				}
 			}//End error function
 		});//End outer Ajax call
 	}//End getCurrentTemp()
 
 	getCurrentTemp();
+}
+
+runProgram();
 });
